865404cb65406aa3096c5011cb8c90cef219be44
Jimmy Shultz <jimmyshultz3@gmail.com>
2024-02-15 14:54:11 -0600

    Made various changes to push the sorted linked list to a stack and then pop the stack to be printed to the terminal element by element.

--- README.md
+++ README.md
@@ -14,7 +14,9 @@ The workspace contains two main folders, where:
 To run the program in the terminal navigate to the src folder and run two commands, `javac App.java` and then `java App`.
 
 ## Generating Documentation
-To generate javadocs navigate to the doc folder (first create one if it has not been created yet) and run `javadoc ../src/App.java`
+To generate javadocs navigate to the doc folder (first create one if it has not been created yet) and run `javadoc ../src/StackCreator/App.java`
 
 ## Version Updates
+1.2 - Change package name to StackCreator.  Add a method to push sorted Linked List elements to a stack, use that method in the main method, and pop the each element to be printed back to the user in the terminal.  Remove original printing of the Linked List.  Rearrange the main method to intialize first and then perform functionality.  Add constant list length to ensure consistency.
+
 1.1 - Create a package for the App class and fix Exception warning and constructor warning that arose when generating documentation in version 1.0
--- src/StackCreator/App.java
+++ src/StackCreator/App.java
@@ -1,60 +1,83 @@
-package LinkedListCreator;
+package StackCreator;
 
 import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.ListIterator;
+import java.util.Stack;
 
 /**
  * Main class to allow the user to input 5 integers, store them in a sorted
- * linked list while adding them, and then eventually printed the sorted
- * linked list back to the user.
+ * linked list while adding them, push the elements of the linked list onto 
+ * a stack in order from smallest to largest, and then pop the largest 
+ * while printing it to the terminal.
  * 
- * The program makes use of the LinkedList, ListIterator, and Scanner packages.
+ * The program makes use of the LinkedList, ListIterator, Stack, and Scanner 
+ * packages.
  * 
  * To run the program in the terminal navigate to the src folder and run two
- * command `javac App.java` and then `java App`.
+ * command `javac StackCreator/App.java` and then `java StackCreator/App`.
  * 
  * To generate javadocs navigate to the doc folder and run 
- * `javadoc ../src/App.java`
+ * `javadoc ../src/StackCreator/App.java`
  * 
  * @author Jimmy Shultz
- * @version 1.1
- * @since 1.0
+ * @version 1.2
+ * @since 1.2
  */
 public class App {
     /**
      * Default contructor used for intilization of the class becuase no 
      * customization is needed.
+     * @since 1.1
      */
     public App () {}
     
     /**
-     * Main method of the class calling the getIntegerInput and addSortedNumber
-     * methods.
+     * Main method of the class calling the getIntegerInput, addSortedNumber, 
+     * and pushLinkedListToStack methods for the length of the list 
+     * (which is currently 5).  Then popping each integer off the stack 
+     * and printing it to the terminal.
+     * 
      * @param args command line arguments are unused in the program.
+     * @since 1.2
      */
     public static void main(String[] args) {
         
         LinkedList<Integer> numbers = new LinkedList<>();
+        Stack<Integer> numbersStack = new Stack<>();
+        Integer listLength = 5;
+
         System.out.println(
             "Please enter 5 integers to be sorted in a linked list");
+
         Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < listLength; i++) {
             int convertedNumber = getIntegerInput(sc);
             addSortedNumber(numbers, convertedNumber);
         }
 
         sc.close();
 
-        System.out.println(numbers);
+        for (int i = 0; i < listLength; i++) {
+            pushLinkedListElementToStack(numbers, numbersStack);
+        }
+
+        System.out.println("");
+        System.out.println("Your sorted stack from top to bottom is:");
+
+        for (int i = 0; i < listLength; i++) {
+            System.out.println(numbersStack.pop());
+        }
         
     }
 
     /**
      * Prompts the user for input and returns an integer.
+     * 
      * @param scanner the scanner that allows for user input
      * @return the user input as an integer
+     * @since 1.0
      */
     public static int getIntegerInput(Scanner scanner) {
 
@@ -78,8 +101,10 @@ public class App {
     /**
      * Adds a number to a linked list while ensuring the linked list stays 
      * sorted.
+     * 
      * @param pNumbers the current linked list
      * @param addedNumber the number to be added
+     * @since 1.0
      */
     public static void addSortedNumber(
         LinkedList<Integer> pNumbers, int addedNumber) {
@@ -95,5 +120,20 @@ public class App {
             }
         }
         iterator.add(addedNumber);
+    }
+
+    /**
+     * Pushes the first element of a linked list of integers 
+     * onto a stack of integers.
+     * 
+     * @param pNumbers
+     * @param pNumbersStack
+     * @since 1.2
+     */
+    public static void pushLinkedListElementToStack (
+        LinkedList<Integer> pNumbers, Stack<Integer> pNumbersStack) {
+        
+        int currentNumber = pNumbers.removeFirst();
+        pNumbersStack.push(currentNumber);
     }
 }
